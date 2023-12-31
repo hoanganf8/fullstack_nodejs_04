cd44c6ea1c7723c22bf62383df5c3f5f1f54f6df
Hoàng An F8 <hoangan@fullstack.edu.vn>
2023-12-31 18:06:20 +0700

    Update code

--- hocnodejs/node02/controllers/auth.controller.js
+++ hocnodejs/node02/controllers/auth.controller.js
@@ -1,7 +1,15 @@
+import { object, string } from "yup";
+
 const getError = (errors, fieldName) => {
   if (errors.length) {
     errors = errors[0];
     return errors[fieldName];
+  }
+};
+const getOld = (old, fieldName) => {
+  if (old.length) {
+    old = old[0];
+    return old[fieldName];
   }
 };
 const authController = {
@@ -11,24 +19,27 @@ const authController = {
     // delete req.session.msg;
     // const msg = req.flash("msg");
     const errors = req.flash("errors");
+    const old = req.flash("old");
 
     res.render("auth/login", {
       layout: "auth-layout",
       // msg,
       getError,
       errors,
+      getOld,
+      old,
     });
   },
-  handleLogin: (req, res) => {
-    const { email, password } = req.body;
-    const errors = {};
-    if (!email) {
-      errors.email = "Vui lòng nhập email";
-    }
-    if (!password) {
-      errors.password = "Vui lòng nhập mật khẩu";
-    }
-    req.flash("errors", errors);
+  handleLogin: async (req, res) => {
+    // const { email, password } = req.body;
+    // const errors = {};
+    // if (!email) {
+    //   errors.email = "Vui lòng nhập email";
+    // }
+    // if (!password) {
+    //   errors.password = "Vui lòng nhập mật khẩu";
+    // }
+    // req.flash("errors", errors);
     //Validate form
     //Check login --> Học sau
     //Thông báo kết quả
@@ -38,6 +49,39 @@ const authController = {
     */
     // req.session.msg = "Đăng nhập thất bại";
     // req.flash("msg", "Đăng nhập thành công");
+
+    //Yup
+    const schema = object({
+      email: string()
+        .required("Email bắt buộc phải nhập")
+        .email("Email không đúng định dạng"),
+      password: string().required("Mật khẩu bắt buộc phải nhập"),
+      name: string()
+        .min(5, "Tên phải từ 5 ký tự")
+        .required("Tên bắt buộc phải nhập")
+        .test("name-upper", "Tên bắt buộc là chữ in HOA", (value) => {
+          return value === value.toUpperCase();
+        }),
+      phone: string()
+        .matches(/^(0|\+84)\d{9}$/, "Số điện thoại không đúng định dạng")
+        .required("Số điện thoại bắt buộc phải nhập"),
+    });
+    try {
+      const body = await schema.validate(req.body, {
+        abortEarly: false,
+      });
+      console.log(body);
+    } catch (e) {
+      const errors = Object.fromEntries(
+        e.inner.map((item) => {
+          return [item.path, item.message];
+        }),
+      );
+
+      req.flash("errors", errors);
+      req.flash("old", req.body);
+    }
+
     return res.redirect("/dang-nhap");
   },
   handleLogout: (req, res) => {
